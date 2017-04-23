32fa331626e109be08ae2c7d16d7ab41c49def90
tonygod <tonygod@hotmail.com>
2017-04-23 13:32:45 -0700

    Added Title scene Added UI with buttons to start game, select level, see credits Baked lightmaps for Level1, Leve2 scenes

---
+++ Assets/antivirus/Scripts/SceneMgr.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMgr : MonoBehaviour {
+
+	void Awake()
+	{
+		DynamicGI.UpdateEnvironment();
+	}
+
+	// Use this for initialization
+	public void NextScene()
+	{
+		int nextIndex = SceneManager.GetActiveScene().buildIndex + 1;
+		if ( nextIndex > SceneManager.sceneCountInBuildSettings )
+		{
+			// just reload same scene if at last scene
+			nextIndex--;
+		}
+		SceneManager.LoadScene( nextIndex );
+	}
+	
+	public void LastScene()
+	{
+		int lastIndex = SceneManager.GetActiveScene().buildIndex - 1;
+		if ( lastIndex < 0 )
+		{
+			// just reload if first scene
+			lastIndex++;
+		}
+		SceneManager.LoadScene( lastIndex );
+	}
+
+	public void LoadScene(string sceneName)
+	{
+		SceneManager.LoadScene(sceneName);
+	}
+}
